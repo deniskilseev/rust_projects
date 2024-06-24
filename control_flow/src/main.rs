97683f131d98270202ff362264716b2c7381a902
Denis Kilseev <deniskilseev1@gmail.com>
2024-06-23 23:28:51 -0700

    loops playing

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -26,17 +26,37 @@ fn main() {
     
     println!("The value of number is: {number}");
 
-    fn main() {
-        let mut counter = 0;
-    
-        let result = loop {
-            counter += 1;
-    
-            if counter == 10 {
-                break counter * 2;
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+
+
+
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
             }
-        };
-    
-        println!("The result is {result}");
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
     }
+    println!("End count = {count}");
 }
